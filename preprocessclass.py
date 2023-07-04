fec467c04972bf4cd500e8d24745d811a5e00d0a
Mucahit <45368415+mucahitunsal@users.noreply.github.com>
2023-07-04 09:44:20 +0300

    Update preprocessclass.py

--- preprocessclass.py
+++ preprocessclass.py
@@ -6,51 +6,80 @@ class ImagePreprocessor:
     def __init__(self):
         pass
 
-    def rescale(self, image, scale_percent=0.8):
-        width = int(image.shape[1] * scale_percent)
-        height = int(image.shape[0] * scale_percent)
+    # Frame'i yeniden boyutlandırma
+    def rescale(self, frame, scale_percent=0.8):
+        width = int(frame.shape[1] * scale_percent)
+        height = int(frame.shape[0] * scale_percent)
         dim = (width, height)
-        return cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 
-    def gray_filter(self, image):
-        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        return cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
 
-    def straighten_image(self, image):
-        # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(image, 50, 150, apertureSize=3)
-        lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
+    # Frame'e gri filtre uygulama
+    def gray_filter(self, frame):
 
-        if lines is not None:
-            for line in lines:
-                rho, theta = line[0]
-                if theta > np.pi / 180 * 80 and theta < np.pi / 180 * 100:
-                    a = np.cos(theta)
-                    b = np.sin(theta)
-                    x0 = a * rho
-                    y0 = b * rho
-                    x1 = int(x0 + 1000 * (-b))
-                    y1 = int(y0 + 1000 * (a))
-                    x2 = int(x0 - 1000 * (-b))
-                    y2 = int(y0 - 1000 * (a))
-                    cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                    break
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        return image
 
-    def thresholding(self, image, threshold_value=127):
-        _, thresh = cv2.threshold(image, threshold_value, 255, cv2.THRESH_BINARY)
+    # Frame'e thresholding işlemi uygulamak
+    def thresholding(self, frame, threshold_value=127):
+        _, thresh = cv2.threshold(frame, threshold_value, 255, cv2.THRESH_BINARY)
+
         return thresh
 
-    def remove_noise(self, image):
-        return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    # Frame üzerindeki gürültüyü azaltmak
+    def remove_noise(self, frame):
 
-    def thin_font(self, image):
+        return cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    # Frame üzerindeki yazıları inceltmek
+    def thin_font(self, frame):
         kernel = np.ones((1, 1), np.uint8)
-        return cv2.erode(image, kernel, iterations=1)
 
-    def thick_font(self, image):
+        return cv2.erode(frame, kernel, iterations=1)
+
+    # Frame üzerindeki yazıları kalınlaştırmak
+
+    def thick_font(self, frame):
         kernel = np.ones((1, 1), np.uint8)
-        return cv2.dilate(image, kernel, iterations=1)
 
-    def remove_borders(self, image, border_size=10):
-        return image[border_size:-border_size, border_size:-border_size]
+        return cv2.dilate(frame, kernel, iterations=1)
+
+    # Frame'in fazlalık kısımlarını atmak
+
+    def remove_borders(self, frame):
+        contours, hierarchy = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours_sorted = sorted(contours, key=lambda x: cv2.contourArea(x))
+        cnt = contours_sorted[-1]
+        x, y, w, h = cv2.boundingRect(cnt)
+        crop = frame[y:y+h, x:x+w]
+
+        return crop
+
+    # Frame'in perspektifini düzeltmek
+
+    def correct_perspective(self, frame):
+
+        corner_detection_params = dict(
+            blockSize=2,
+            ksize=3,
+            k=0.04
+        )
+
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        corners = cv2.cornerHarris(gray_frame, **corner_detection_params)
+
+        corners = cv2.dilate(corners, None)
+
+        threshold = 0.01 * corners.max()
+        corners_indices = np.where(corners > threshold)
+        corners = np.float32(np.column_stack((corners_indices[1], corners_indices[0])))
+
+        src_points = corners
+        dst_points = np.float32([[0, 0], [800, 0], [800, 800], [0, 800]])
+        perspective_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
+
+        corrected_image = cv2.warpPerspective(frame, perspective_matrix, (800, 800))
+
+        return corrected_image
+
